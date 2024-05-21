0f3d2a4510c995a34a323252a8d86e2969a78210
eulogioep <eulogio-ep@hotmail.com>
2024-05-21 15:40:33 +0200

    Condiciones while y do...while

--- 01-Introduccion_Kotlin/app/src/main/java/com/eulogioep/introduccionakotlin/WhileYDoWhile.kt
+++ 01-Introduccion_Kotlin/app/src/main/java/com/eulogioep/introduccionakotlin/WhileYDoWhile.kt
@@ -1,4 +1,26 @@
 package com.eulogioep.introduccionakotlin
 
-class WhileYDoWhile {
+fun main(){
+
+    // while con Kotlin
+    var i = 1
+
+    while(i <= 5){
+        println("Valor: $i")
+        i++
+    }
+
+    // do while con Kotlin.
+    var sum = 0
+    var input: String
+
+    do{
+        print("Ingrese un numero: ")
+        input = readLine()!! // Los dos "!!" indica que el valor no puede ser nulo.
+        sum += input.toInt() // Transforma el valor a entero.
+
+    }while(input != "0") // Si el valor de input es 0, se sale del bucle.
+
+    println("La suma es: $sum")
+
 }

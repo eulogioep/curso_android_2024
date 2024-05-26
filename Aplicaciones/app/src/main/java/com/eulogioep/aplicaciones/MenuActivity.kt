af84d8a9d8f80e235dc1519774be7f6868e7a25f
eulogioep <eulogio-ep@hotmail.com>
2024-05-26 15:57:20 +0200

    Creado el menú de las apps.

--- Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/MenuActivity.kt
+++ Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/MenuActivity.kt
@@ -1,10 +1,13 @@
 package com.eulogioep.aplicaciones
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.eulogioep.aplicaciones.firstapp.FirstAppActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,22 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val btnSaludos = findViewById<AppCompatButton>(R.id.btnSaludos)
+
+        btnSaludos.setOnClickListener {
+            navigateToSaludosAPP()
+        }
     }
+
+
+    private fun navigateToSaludosAPP() {
+
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+
+
+    }
+
+
 }

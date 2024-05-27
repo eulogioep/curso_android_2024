b0a4ef8ae413f9a2a6e98ee95e7ce4f90c3b84bd
eulogioep <eulogio-ep@hotmail.com>
2024-05-27 14:14:09 +0200

    IMC App: Inicio Altura.

--- Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/imccalc/IMCActivity.kt
+++ Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/imccalc/IMCActivity.kt
@@ -3,11 +3,23 @@ package com.eulogioep.aplicaciones.imccalc
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.eulogioep.aplicaciones.R
 
 class IMCActivity : AppCompatActivity() {
+
+    // Variables
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    // Variables de los CardView
+    // lateinit indica que se inicializará más adelante ya que no funcionaría fuera del método onCreate.
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +29,63 @@ class IMCActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Inicializar las variables de los CardView
+        initComponents()
+        // Inicializar los listeners de los CardView
+        initListeners()
+        // Inicializar la UI
+        initUI()
+
     }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+    }
+
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+
+        val colorReference = if (isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
+    }
+
+
+
+
+
+
 }

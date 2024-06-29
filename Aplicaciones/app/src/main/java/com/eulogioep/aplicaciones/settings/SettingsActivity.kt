335f40db72213ff93d80f98f120359d9646f47d2
eulogioep <eulogio-ep@hotmail.com>
2024-06-29 15:50:35 +0200

    Settings App: Inicio

---
+++ Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/settings/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.eulogioep.aplicaciones.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.eulogioep.aplicaciones.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+    }
+}

d378333108df671b17c8f2b155b19824752eda9a
eulogioep <eulogio-ep@hotmail.com>
2024-07-09 17:46:01 +0200

    Settings App: Iniciando la persistencia de datos.

--- Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/settings/SettingsActivity.kt
+++ Aplicaciones/app/src/main/java/com/eulogioep/aplicaciones/settings/SettingsActivity.kt
@@ -1,12 +1,32 @@
 package com.eulogioep.aplicaciones.settings
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.eulogioep.aplicaciones.R
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.eulogioep.aplicaciones.databinding.ActivitySettingsBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+// Función de extensión del Context. El by es un delegado que nos permite crear una única instancia
+// de la BBDD (Singleton).
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 
 class SettingsActivity : AppCompatActivity() {
+
+    companion object {
+        const val VOLUME_LVL = "volune_lvl"
+        const val KEY_BLUETOOTH = "key_bluetooth"
+        const val KEY_VIBRATION = "key_vibration"
+        const val KEY_DARK_MODE = "key_dark_mode"
+    }
 
     private lateinit var binding: ActivitySettingsBinding
 
@@ -14,6 +34,47 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+
+    }
+
+    private fun initUI() {
+        binding.rsVolume.addOnChangeListener { _, value, _ ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveVolume(value.toInt())
+            }
+
+        }
+
+        binding.switchBluetooth.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_BLUETOOTH, isChecked)
+            }
+        }
+
+        binding.switchVibration.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_VIBRATION, isChecked)
+            }
+        }
+
+        binding.switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            CoroutineScope(Dispatchers.IO).launch {
+                saveOptions(KEY_DARK_MODE, isChecked)
+            }
+        }
+    }
+
+    private suspend fun saveVolume(value: Int) {
+        dataStore.edit { preferences ->
+            preferences[intPreferencesKey(VOLUME_LVL)] = value
+        }
+    }
+
+    private suspend fun saveOptions(key: String, value: Boolean) {
+        dataStore.edit { preferences ->
+            preferences[booleanPreferencesKey(key)] = value
+        }
 
     }
 }
